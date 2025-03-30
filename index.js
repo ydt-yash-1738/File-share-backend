450a8d58436d230267adf5c3a10306152fb70cda
Yashdev Tiwari <yashdt50@gmail.com>
2025-03-30 15:03:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(cors({
-  origin: 'https://filexchange-ydt.vercel.app/',
+  origin: 'https://filexchange-ydt.vercel.app',
   credentials: true,
 }));
 
