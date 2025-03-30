bd6aef1031f4664777183e945d5a259806207751
Yashdev Tiwari <yashdt50@gmail.com>
2025-03-30 17:01:38 +0530

    Update file.js

--- models/file.js
+++ models/file.js
@@ -1,20 +1,52 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
+
+// const FileSchema = new mongoose.Schema({
+//     path: {
+//         type: String,
+//         required: true
+//     },
+//     name: {
+//         type: String,
+//         required: true
+//     },
+//     downloadCount: {
+//         type: Number,
+//         required: true,
+//         default: 0
+//     }
+// })
+
+// const File = mongoose.model('file', FileSchema);
+
+// export default File;
+
+import mongoose from 'mongoose';
 
 const FileSchema = new mongoose.Schema({
-    path: {
-        type: String,
-        required: true
-    },
     name: {
         type: String,
         required: true
     },
+    path: {
+        type: String,
+        required: true
+    },
+    size: {
+        type: Number,
+        default: 0
+    },
+    type: {
+        type: String
+    },
     downloadCount: {
         type: Number,
-        required: true,
         default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
     }
-})
+});
 
 const File = mongoose.model('file', FileSchema);
 
