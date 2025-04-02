e746d667a6274806e62773cab81e285f792e8ece
Yashdev Tiwari <yashdt50@gmail.com>
2025-04-02 17:19:57 +0530

    Update auth.controller.js

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -15,24 +15,57 @@ export const signup = async (req, res, next) => {
   }
 };
 
+// export const signin = async (req, res, next) => {
+//   const { email, password } = req.body;
+//   try {
+//     const validUser = await User.findOne({ email });
+//     if (!validUser) return next(errorHandler(404, 'User not found'));
+//     const validPassword = bcryptjs.compareSync(password, validUser.password);
+//     if (!validPassword) return next(errorHandler(401, 'wrong credentials'));
+//     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+//     const { password: hashedPassword, ...rest } = validUser._doc;
+//     const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+//     res
+//       .cookie('access_token', token, { httpOnly: true, expires: expiryDate })
+//       .status(200)
+//       .json(rest);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(404, 'User not found'));
+
     const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validPassword) return next(errorHandler(401, 'wrong credentials'));
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    if (!validPassword) return next(errorHandler(401, 'Wrong credentials'));
+
+    // Check if the logged-in user is the admin
+    const isAdmin = email === process.env.ADMIN_EMAIL;
+
+    // Generate JWT token including isAdmin field
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    );
+
     const { password: hashedPassword, ...rest } = validUser._doc;
-    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour expiration
+
     res
       .cookie('access_token', token, { httpOnly: true, expires: expiryDate })
       .status(200)
-      .json(rest);
+      .json({ ...rest, isAdmin }); // Return isAdmin status in response
+
   } catch (error) {
     next(error);
   }
 };
+
 
 export const google = async (req, res, next) => {
   try {
@@ -81,3 +114,5 @@ export const google = async (req, res, next) => {
 export const signout = (req, res) => {
   res.clearCookie('access_token').status(200).json('Signout success!');
 };
+
+
